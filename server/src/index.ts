abad557348292089bfd54494e097a692f2720b9b
Flavio Scimeca <flavioscimeca3@gmail.com>
2023-05-08 17:57:52 +0200

    created /get-posts endpoint

--- server/src/index.ts
+++ server/src/index.ts
@@ -2,10 +2,13 @@ import express, { Request, Response } from 'express';
 
 const app = express();
 
-app.get('/', (req: Request, res: Response) => {
-  res.send('Typescript and node');
+//! creo un end-point da richiamare lato client che ritorna i dati di quell API (uso una GET)
+app.get('/get-posts', (req: Request, res: Response) => {
+  fetch('https://22hbg.com/wp-json/wp/v2/posts/')
+    .then((res) => res.json())
+    .then((response) => res.send(response));
 });
 
-app.listen(4321, () => {
-  console.log('running on 4321');
+app.listen(4000, () => {
+  console.log('running on 4000');
 });
