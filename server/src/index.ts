8e08f137eff52c0667da979d0fe34185a57e5ff0
Flavio Scimeca <flavioscimeca3@gmail.com>
2023-05-08 22:37:48 +0200

    creato l endpoint /posts-filtered con parametri statici (al momento)

--- server/src/index.ts
+++ server/src/index.ts
@@ -2,11 +2,27 @@ import express, { Request, Response } from 'express';
 
 const app = express();
 
-//! creo un end-point da richiamare lato client che ritorna i dati di quell API (uso una GET)
-app.get('/get-posts', (req: Request, res: Response) => {
+//! creo un endpoint da richiamare lato client che ritorna i dati di quell API (uso una GET)
+app.get('/posts', (req: Request, res: Response) => {
   fetch('https://22hbg.com/wp-json/wp/v2/posts/')
     .then((res) => res.json())
     .then((response) => res.send(response));
+});
+
+//! per ora i valori sono statici poi li faro passare dal req.body (voglio capire se posso filtrarli direttamente dalla query)
+app.get('/posts-filtered', (req: Request, res: Response) => {
+  let t: string = 'a';
+  let n: number = 1;
+
+  fetch(`https://22hbg.com/wp-json/wp/v2/posts/`)
+    .then((res) => res.json())
+    .then((response: Array<Object>) =>
+      res.send(
+        response
+          .filter((data: any) => data.title.rendered.includes(t))
+          .splice(0, n ?? response.length - 1)
+      )
+    );
 });
 
 app.listen(4000, () => {
