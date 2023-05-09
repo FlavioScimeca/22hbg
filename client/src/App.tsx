a4906d13bd39580fd50115b072f6e7f2dd5a6e46
Flavio Scimeca <flavioscimeca3@gmail.com>
2023-05-09 14:17:32 +0200

    creata la prima interfaccia lato client per fetchare tutti i dati dell api tramite l endpoint /posts

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,35 +1,33 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useEffect, useState } from 'react';
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [data, setData] = useState<any[]>([]);
 
+  const fetchApi = () => {
+    fetch(`${import.meta.env.VITE_URL_KEY}/posts`)
+      .then((res) => res.json())
+      .then((resp) => setData([...data, resp]))
+      .catch((err) => console.log(err));
+  };
+
+  useEffect(() => {
+    fetchApi();
+    console.log(data);
+  }, []);
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <h2>api</h2>
+      {data.length > 0 ? (
+        data[0].map((el: any, idx: number) => (
+          <div key={idx}>
+            <p>{el.title.rendered}</p>
+          </div>
+        ))
+      ) : (
+        <div>non fetchati</div>
+      )}
     </>
-  )
+  );
 }
 
-export default App
+export default App;
